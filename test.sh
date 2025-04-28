ea6168989db1da7d2a25139aa09fcfdc8f9e1371
Petraea Gillespie <pgillesp@uoregon.edu>
2025-04-28 14:48:02 -0700

    project 1 stuff

---
+++ test.sh
@@ -0,0 +1,25 @@
+killall -9 kv_service
+
+echo "starting service..."
+
+./service/tools/kv/server_tools/start_kv_service.sh
+
+echo "service started."
+
+SECONDS=0
+SRV_TOOL="bazel-bin/service/tools/kv/api_tools/kv_service_tools"
+CONF="service/tools/config/interface/service.config"
+
+echo "beginning basic benchmark"
+
+while [ $SECONDS -le 100 ]
+do
+    for j in $(seq 1 1000)
+    do
+    $SRV_TOOL --config $CONF --cmd set --key $j --value $SECONDS
+    done
+done
+
+# killall -9 kv_service
+
+echo "done"
